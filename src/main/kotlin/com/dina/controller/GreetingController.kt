6427c713dee9c36a28dee4615cea46cff7e0a41e
dinesh <dindina@gmail.com>
2025-06-19 12:37:56 +1000

    added klogging

--- src/main/kotlin/com/dina/controller/GreetingController.kt
+++ src/main/kotlin/com/dina/controller/GreetingController.kt
@@ -1,6 +1,7 @@
 package com.dina.controller
 
 import com.dina.service.GreetingsService
+import mu.KLogging
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
@@ -9,8 +10,11 @@ import org.springframework.web.bind.annotation.RestController
 @RequestMapping("/v1/greetings")
 class GreetingController(val service: GreetingsService) {
 
+    companion object: KLogging()
+
     @RequestMapping("/{name}")
     fun sayHello(@PathVariable("name") name :String) :String {
+        logger.info("in controller $name")
         return service.getGreeting(name);
 
 
